8fe5e7e7bda59ef11bd6d8dcf7260992b2462083
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-03 10:34:39 -0400

    Remove all default Omarchy web apps

--- configs/common/remove-default-apps.sh
+++ configs/common/remove-default-apps.sh
@@ -2,7 +2,19 @@
 
 # Remove only installed web apps. `omarchy-webapp-remove` accepts one app name
 # per invocation, so passing the list as separate arguments would form one name.
-for webapp in "Basecamp" "Google Messages" "Google Photos" "HEY"; do
+for webapp in \
+  "Basecamp" \
+  "Discord" \
+  "Google Contacts" \
+  "Google Maps" \
+  "Google Messages" \
+  "Google Photos" \
+  "HEY" \
+  "WhatsApp" \
+  "X" \
+  "YouTube" \
+  "Zoom"
+do
   if [[ -e "$HOME/.local/share/applications/$webapp.desktop" ]]; then
     omarchy-webapp-remove "$webapp" >/dev/null
   fi
