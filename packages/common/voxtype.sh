dad93563e9135f0dfe201c0ef642d6dccbac670d
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-03 14:06:57 -0400

    Update voxtype.sh

--- packages/common/voxtype.sh
+++ packages/common/voxtype.sh
@@ -5,5 +5,5 @@ if ! systemctl --user is-active --quiet voxtype; then
   voxtype setup --no-post-install
   voxtype setup --download --model large-v3-turbo --quiet
   voxtype setup systemd
-  # sudo voxtype setup gpu --enable
+  sudo voxtype setup gpu --enable
 fi
