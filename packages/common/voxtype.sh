b2344e40810229b15bc51556fb62fa86d68c9596
Pierre Olivier Martel <pomartel@coderubik.com>
2026-09-03 14:06:49 -0400

    Update voxtype.sh

--- packages/common/voxtype.sh
+++ packages/common/voxtype.sh
@@ -3,9 +3,7 @@ omarchy-pkg-add wtype voxtype-bin
 
 if ! systemctl --user is-active --quiet voxtype; then
   voxtype setup --no-post-install
+  voxtype setup --download --model large-v3-turbo --quiet
   voxtype setup systemd
-  # voxtype setup --download --model large-v3-turbo --quiet
   # sudo voxtype setup gpu --enable
-
-  omarchy-restart-waybar
 fi
